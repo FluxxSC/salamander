9cf87dead33dd646dadc434d1ad93f0a281c10be
Gekkolav <olav.sule@>
2017-05-09 19:03:02 +0200

    Merge conflict test Olav

--- src/salamander/Main.java
+++ src/salamander/Main.java
@@ -3,6 +3,7 @@ package salamander;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Andre forandringen, fjerna helge sin forandring.");
+		System.out.println("Andre forandringen, fjerna helge sin forandring. LOl get rekt.");
+		System.out.println("Olav sitt linjebidrag");
 	}
 }
