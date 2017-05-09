ab234025cb5ee3c9e06fce571d39e06b6ae20e18
Gekkolav <olav.sule@>
2017-05-09 18:52:47 +0200

    Den andre forandringen

--- src/salamander/Main.java
+++ src/salamander/Main.java
@@ -3,7 +3,6 @@ package salamander;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-		System.out.println("Første forandring.");
+		System.out.println("Andre forandringen, fjerna helge sin forandring.");
 	}
 }
