67a22473c35d4e47e7fbbe43cd3ea5e2dbf4b6c4
Helge Fylkesnes <Helge Fylkesnes>
2017-05-09 19:03:25 +0200

    Min forandring

--- src/salamander/Main.java
+++ src/salamander/Main.java
@@ -3,6 +3,6 @@ package salamander;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Andre forandringen, fjerna helge sin forandring.");
+		System.out.println("Helges linje");
 	}
 }
