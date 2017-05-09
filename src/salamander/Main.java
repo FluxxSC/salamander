8716266246e9e5f6a2df4bb7b68e76960b3a0fcf
Helge Fylkesnes <Helge Fylkesnes>
2017-05-09 19:27:04 +0200

    La til Olavs linje

--- src/salamander/Main.java
+++ src/salamander/Main.java
@@ -3,16 +3,14 @@ package salamander;
 public class Main {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("Helges linje");
 
 		System.out.println("Andre forandringen, fjerna helge sin forandring.");
 		
 		System.out.println("Rune er best!");
 
-=======
-		System.out.println("Andre forandringen, fjerna helge sin forandring. LOl get rekt.");
 		System.out.println("Olav sitt linjebidrag");
->>>>>>> remotes/origin/Olavs_Branch
+
 	}
 }
