54477181242f060fc727c8f72e3a71934df00797
Gekkolav <olav.sule@>
2017-05-18 19:41:55 +0200

    fiksa opp LetterChecking, +masse kommentarer

--- src/salamander/LetterChecking.java
+++ src/salamander/LetterChecking.java
@@ -2,36 +2,116 @@ package salamander;
 
 public class LetterChecking {
 
+	// Lager en liste med alle bokstaver. 
+	
 	private String[] allLetters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
 			"r", "s", "t", "u", "v", "w", "x", "y", "z", "æ", "ø", "å" };
-	private int wrongLetters = 0;
+	private String[] remainingLetters = allLetters;
 
-	private boolean correctLetter = false;
+	// Ubrukt variabel som evt kan lage en liste med de gjettede bokstavene. 
+	
+	private String[] guesedLetters;
+
+	// Variabler som blir brukt i metoden feedWord.
+
+	private String correctWord;
+	private String[] wordProgress;
+	private String[] correctLetters;
+	private int[] correctLetterPlacements;
+	private int correctAmountLetters;
+
+	// Variabler som blir brukt i metoden letterInput.
 
 	private String currentLetter;
+	private String[] correctRemainingLetters;
 
-	public void feedLetter () {
-		private String correctWord = 
+	// Variabler som blir brukt i metoden campareLetters.
+
+	private boolean correctLetter = false;
+	private int wrongLetters = 0;
+
+	// feedWord tar inn et ord (string) og gjør det om til en liste (string) med
+	// bokstaver.
+	// Den lager også en liste som utgjør outputen til det nye ordet typ: _ _ _
+	// _
+
+	public void feedWord(String correctWord) {
+		this.correctWord = correctWord;
+		correctLetters = correctWord.split("");
+		correctLetterPlacements = new int[correctLetters.length];
+		wordProgress = new String[correctLetters.length];
+
+		for (int i = 0; i == correctLetters.length; i++) {
+			wordProgress[i] = "_ ";
+
+		}
 	}
 
-	public void letterInput () {
-		currentLetter = 
+	// Den tar inn bokstaven som blir gjettet som currentLetter, lager en ny
+	// liste med bokstaver der den gjettede bokstaven blir fjernet.
+	// Den setter correctLetter = false hver gang en bokstav blir gjettet før
+	// bokstaven blir sjekket.
+
+	public void letterInput(String currentLetter) {
+		this.currentLetter = currentLetter;
+
+		correctRemainingLetters = new String[remainingLetters.length - 1];
+
+		for (int i = 0; i == remainingLetters.length; i++) {
+			if (currentLetter != remainingLetters[i]) {
+				correctRemainingLetters[i] = remainingLetters[i];
+			}
+		}
+
+		remainingLetters = correctRemainingLetters;
+		correctLetter = false;
+
 	}
 
-	private String[] correctLetters = Str.Split("correctWord");
+	// for løkken sjekker hver bokstav i correctLetters mot den gjettede
+	// bokstaven.
+	// If ==, correctLetterPlacements setter et 1 der riktig bokstav.
+	// If !=, wrongLetters++ som teller antall feil.
+	// Kaller wordProgression dersom correctLetter == true.
+	// Tømmer listen med correctLetterPlacements tilbake til en liste med bare 0
+	// så ikke alle bokstaver byttes ut hver gang.
 
 	public void compareLetters() {
 		for (int i = 0; i == correctLetters.length; i++) {
 			if (correctLetters[i] == currentLetter) {
 				correctLetter = true;
+				correctLetterPlacements[i] = 1;
+
 			}
 		}
 		if (correctLetter == false) {
-			wrongLetters ++;
+			wrongLetters++;
+		} else {
+			wordProgression();
+
+			for (int i = 0; i == correctLetters.length; i++) {
+				correctLetterPlacements[i] = 0;
+			}
+
 		}
 
 	}
-	
-	
+
+	// metoden compareLetters skal kalle denne metoden for å gi den oppdaterte
+	// listen som viser ordgjettingsprogresjonen.
+	// Den går igjennom listen correctLetterPlacements og setter inn currentWord
+	// der == 1.
+
+	public void wordProgression() {
+		for (int i = 0; i == correctLetters.length; i++) {
+			if (correctLetter) {
+				if (correctLetterPlacements[i] == 1) {
+					wordProgress[i] = currentLetter;
+
+				}
+			}
+		}
+
+	}
 
 }
